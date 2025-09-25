6734a90cdca819e11cb39cf0cd0e6fe5292bd2f7
Ethan-Chang-1111 <ethan.m.chang@gmail.com>
2025-09-24 21:18:31 -0500

    MP2 Q2 Part a,b

--- hw2_template/hw2_template/hw2_q2.py
+++ hw2_template/hw2_template/hw2_q2.py
@@ -13,11 +13,43 @@ def gaussian_theta(X, y):
         y (S LongTensor): label of each object, y[i] = 0/1
 
     Returns:
-        mu (2 x N Float Tensor): MAP estimation of mu in N(mu, sigma2)
-        sigma2 (2 x N Float Tensor): MAP estimation of mu in N(mu, sigma2)
+        mu (2 x N Float Tensor): MLE estimation of mu in N(mu, sigma2)
+        sigma2 (2 x N Float Tensor): MLE estimation of mu in N(mu, sigma2)
 
     '''
-    pass
+
+    # Calculate sample mean, mu
+    # Then use to calculate sigma2, sum squared differences, (divided by n-1)
+    # Where X=1, Y=y
+
+    # Outputs are 2xN tensors, for y=0, and y=1
+
+    
+    # print(X.shape)
+    # print(y.unsqueeze(-1).shape)
+    
+    zeros = X[y == 0]
+    print(zeros.shape)
+    ones = X[y == 1]
+    print(ones.shape)
+
+    # output as (var, mean)
+    a = torch.var_mean(zeros, dim=0)
+    print(a)
+
+    b = torch.var_mean(ones, dim=0)
+    print(b)
+
+    # Standard is to have 0 on top, then 1
+    # Shouldn't actually matter 
+
+    mu = torch.stack((a[1], b[1]))
+    sigma2 = torch.stack((a[0], b[0]))
+
+    print(mu.shape)
+    print(sigma2.shape)
+
+    return mu, sigma2
 
 def gaussian_p(y):
     '''
@@ -25,10 +57,18 @@ def gaussian_p(y):
         y (S LongTensor): label of each object
 
     Returns:
+        # Scalar, as per instructions
         p (float or scalar Float Tensor): MLE of P(Y=0)
 
     '''
-    pass
+
+    # num Y=0 divided by number of samples?
+    # Should be done
+
+    numerator = y[y==0]
+    denom = y.size
+    print(numerator.shape)
+    return numerator / denom
 
 def gaussian_classify(mu,sigma2, p, X):
     '''
@@ -42,4 +82,20 @@ def gaussian_classify(mu,sigma2, p, X):
         y (S LongTensor): label of each object for classification, y[i] = 0/1
     
     '''
+
+    # Calculate log-likelihood. Equation should be on the slides, if not, post to campuswire
+
+    
+
     pass
+
+
+def main():
+    # For testing
+    X, y = utils.gaussian_dataset("train", prefix="gaussian")
+    mu, sigma2  = gaussian_theta(X, y)
+    p = gaussian_p(y)
+    
+
+if __name__ == "__main__":
+    main()
